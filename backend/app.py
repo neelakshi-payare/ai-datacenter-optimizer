e349f6e08bd60738d866ac899e23f9aba772957b
Neelakshi Payare <neelakshipayare01@gmail.com>
2026-04-23 11:37:29 +0530

    Fix port to 8080

--- backend/app.py
+++ backend/app.py
@@ -95,5 +95,5 @@ def resources():
     })
 if __name__ == "__main__":
  import os
- port = int(os.environ.get("PORT", 5000))
+ port = int(os.environ.get("PORT", 8080))
  app.run(debug=False, host="0.0.0.0", port=port)
